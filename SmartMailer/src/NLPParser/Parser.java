3bbfd992322f4b67c06280def5e0d750809e73d9
Vikas Chhabra <vikas.chhabra@snapdeal.com>
2015-09-04 15:40:25 +0530

    commiting model

--- SmartMailer/src/NLPParser/Parser.java
+++ SmartMailer/src/NLPParser/Parser.java
@@ -3,7 +3,6 @@ package NLPParser;
 import java.util.List;
 import java.util.Properties;
 
-import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
@@ -11,11 +10,7 @@ import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
 /**
@@ -24,53 +19,43 @@ import edu.stanford.nlp.util.CoreMap;
  */
 public class Parser {
 
-    public static void main(String[] args) {
+    Parser parser = null;
+
+    public Parser getInstance() {
+        if (null == parser) {
+            synchronized (this) {
+                if (null == parser) {
+                    parser = new Parser();
+                }
+            }
+        }
+        return parser;
+    }
+
+    public void parse() {
         Properties props = new Properties();
         props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, sentiment");
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
-        // read some text in the text variable
         String text = "I am happy";
-
-        // create an empty Annotation just with the given text
         Annotation document = new Annotation(text);
-
-        // run all Annotators on this text
         pipeline.annotate(document);
-
-        // these are all the sentences in this document
-        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
         List<CoreMap> sentences = document.get(SentencesAnnotation.class);
 
         for (CoreMap sentence : sentences) {
-            // traversing the words in the current sentence
-            // a CoreLabel is a CoreMap with additional token-specific methods
             for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
-                // this is the text of the token
                 String word = token.get(TextAnnotation.class);
-                // this is the POS tag of the token
                 String pos = token.get(PartOfSpeechAnnotation.class);
-                // this is the NER label of the token
-                String ne = token.get(NamedEntityTagAnnotation.class);
-                System.out.println("word:" + word + " pos:" + pos + " ne:" + ne);
+                //                String ne = token.get(NamedEntityTagAnnotation.class);
+                //                System.out.println("word:" + word + " pos:" + pos + " ne:" + ne);
             }
-
-            // this is the parse tree of the current sentence
-            Tree tree = sentence.get(TreeAnnotation.class);
-
-            // this is the Stanford dependency graph of the current sentence
-            SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
-            System.out.println(tree);
-            System.out.println(dependencies);
+            //            Tree tree = sentence.get(TreeAnnotation.class);
+            //            SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
+            //            System.out.println(tree);
+            //            System.out.println(dependencies);
             String sentiment = sentence.get(SentimentCoreAnnotations.ClassName.class);
             System.out.println(sentiment + "\t" + sentence);
         }
-
-        // This is the coreference link graph
-        // Each chain stores a set of mentions that link to each other,
-        // along with a method for getting the most representative mention
-        // Both sentence and token offsets start at 1!
-        //        Map<Integer, CorefChain> graph = document.get(CorefChainAnnotation.class);
     }
 
 }
